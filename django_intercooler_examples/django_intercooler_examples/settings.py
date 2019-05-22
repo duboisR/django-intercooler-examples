5e2cf7ced2f31dc775b96ed1357db79d99780120
DUBOIS Romain <dubois.rom@gmail.com>
2019-05-22 23:05:15 +0200

    update settings Template dirs value

--- django_intercooler_examples/django_intercooler_examples/settings.py
+++ django_intercooler_examples/django_intercooler_examples/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'django_intercooler_examples.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates', ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
